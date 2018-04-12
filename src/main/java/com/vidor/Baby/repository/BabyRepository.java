d7fca404df4500f3de67ead18ee4e9d4711afe4c
wenwu.chen <wenwu.chen@openjawtech.com>
2018-04-12 13:58:03 +0800

    hibernate ehcache

--- src/main/java/com/vidor/Baby/repository/BabyRepository.java
+++ src/main/java/com/vidor/Baby/repository/BabyRepository.java
@@ -8,8 +8,15 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface BabyRepository extends JpaRepository<Baby, Integer>, BabyRepositoryCustom{
+    /**
+     * can't cache all methods here and ones in JpaRepository
+     * @param age
+     * @return
+     */
+    //can't cache List
     public List<Baby> findByAge(Integer age);
 
     @Query("from Baby b where b.age=:age")
+    //can't cache entity
     public Baby findBaby(@Param("age") Integer age);
 }
--- src/main/java/com/vidor/Baby/repository/BabyRepositoryCustom.java
+++ src/main/java/com/vidor/Baby/repository/BabyRepositoryCustom.java
@@ -1,5 +1,7 @@
 package com.vidor.Baby.repository;
 
+import com.vidor.Baby.entity.Baby;
+
 import java.util.List;
 
 public interface BabyRepositoryCustom{
@@ -12,4 +14,8 @@ public interface BabyRepositoryCustom{
     List<Object[]> groupByBabyAsHql();
     //基于Specification的方式进行查询，使用的是CriteriaQuery进行查询
     List<Object[]> groupBybabyAsSpecification(Integer number);
+    //cache List
+    List<Baby> findByAgeCache(Integer age);
+    //cache Entity
+    Baby findByIdCache(Integer id);
 }
